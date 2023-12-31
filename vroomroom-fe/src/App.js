695fd0e286b032a1a19dcca336f51f39dc781d4a
m0hx777 <m0hx@protonmail.com>
2023-12-31 13:55:38 +0300

    uwu

--- vroomroom-fe/src/App.js
+++ vroomroom-fe/src/App.js
@@ -1,12 +1,19 @@
 import Home from './components/home/Home';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Axios  from 'axios';
+import CategoryList from './components/home/CategoryList';
 
 function App() {
    return <Home />;
   
-
+    <div>
+      <Routes>
+        <Route path="/" element={ isAuth ? <AuthorList></AuthorList> : <Signin login={loginHandler}></Signin>}></Route>
+      </Routes>
+    </div>
 }
 
 export default App;
---
+++ vroomroom-fe/src/components/home/CategoryList.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+import Axios from 'axios'
+import Category from './Category'
+import CategoryCreateForm from "./CategoryCreateForm";
+
+export default function CategoryList() {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // Call API
+    loadCategoryList()
+  }, [])
+
+  const loadCategoryList = () => {
+    Axios.get("category/index")
+    .then((response) => {
+      console.log(response)
+      setCategories(response.data.categories)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const addCategory = (category) => {
+    Axios.post("category/add", category)
+    .then( res => {
+      console.log("Category Added successfully!");
+      loadCategoryList();
+    })
+    .catch( err => {
+      console.log("Error adding Category!");
+      console.log(err);
+    })
+  }
+  
+
+  const allcategories = categories.map((category, index) => (
+    <tr key={index}>
+      <Category {...category}/>
+    </tr>
+  ))
+
+  return (
+    <div>
+      <h1>Category List</h1>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+            </tr>
+            {allcategories}
+          </tbody>
+        </table>
+      </div>
+      <CategoryCreateForm addCategory = {addCategory}></CategoryCreateForm>
+      </div>
+  )
+}
