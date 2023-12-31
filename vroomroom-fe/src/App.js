b9f9ca39af360e84cbece3b0083bbb1288ff819e
ALio <x3liox1@gmail.com>
2023-12-31 15:23:44 +0300

    fe signup

--- vroomroom-fe/src/App.js
+++ vroomroom-fe/src/App.js
@@ -1,12 +1,114 @@
+import React, { useEffect, useState } from 'react'
 import Home from './components/home/Home';
-import logo from './logo.svg';
+import Signup from './components/home/user/Signup';
+import Signin from './components/home/user/Signin';
+import {Routes, Route, Link} from "react-router-dom";
+import Axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {jwtDecode} from 'jwt-decode'
+import WishList from './components/home/WishList';
 
 function App() {
-   return <Home />;
-  
-
-}
+   const [isAuth, setIsAuth] = useState(false);
+   const [user, setUser] = useState({});
+ 
+   useEffect(() => {
+  const user = getUser();
+  console.log(user);
+  if(user){
+   setIsAuth(true);
+   setUser(user);
+  }else{
+   localStorage.removeItem("token");
+   setIsAuth(false);
+   setUser(null); 
+  }
+ },[])
+ 
+   const registerHandle = (user) => {
+     Axios.post("auth/signup", user)
+     .then(res => {
+       console.log(res);
+     })
+     .catch(err => {
+       console.log(err);
+     })
+   }
+ 
+   const loginHandle = (cred) => {
+     Axios.post("auth/signin", cred)
+     .then( res => {
+       console.log(res.data.token);
+       let token = res.data.token;
+ 
+       if(token != null) 
+       {
+         localStorage.setItem("token", token);
+         const user = getUser();
+         console.log(user);
+         user ? setIsAuth(true) : setIsAuth(false)
+         user ? setUser(user) : setUser(null)
+ 
+       }
+     })
+     .catch(err => {
+       console.log(err);
+     })
+   }
+ 
+   const getUser =() => {
+ const token = getToken();
+ return token ? jwtDecode(token).user : null 
+   }
+ 
+   const getToken = () => {
+     const token = localStorage.getItem("token");
+     return token;
+ 
+   }
+   const onLogoutHandle = (e) => {
+     e.preventDefault();
+     localStorage.removeItem("token");
+     setIsAuth(false);
+     setUser(null);
+   }
+ 
+   return (    
+      
+     <div>
+       <nav>
+         
+         {isAuth ? 
+         (
+           <div>
+           <Link to="/">Home</Link> &nbsp;
+           <Link to="/logout" onClick={onLogoutHandle}>Logout</Link>
+         </div>
+         ) : 
+         (
+           <div>
+           <Link to="/">Home</Link> &nbsp;
+           <Link to="/signup">Signup</Link> &nbsp;
+           <Link to="/signin">Signin</Link> &nbsp;
+         </div>
+         )
+       }
+         
+       </nav>
+       <div>
+       <Routes>
+           <Route path="/" element={ isAuth ? <Home /> : <Signin login={loginHandle}></Signin>}></Route>
+           <Route path='/signup' element={<Signup register={registerHandle}></Signup>}></Route>
+           <Route path='/signin' element ={ isAuth ? <Home/> : <Signin login={loginHandle}></Signin>}></Route>
+           {/* <Route path="/wishlist/show" element={ isAuth ? <WishList></WishList> : <Signin login={loginHandle}></Signin>}></Route> */}
+           
+         </Routes>
+       </div>
+       <footer></footer>
+     </div>
+   )
+ }
+ 
 
 export default App;
--- vroomroom-fe/src/components/home/Home.jsx
+++ vroomroom-fe/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 class Home extends React.Component{
     render() {
         return (
@@ -8,14 +9,14 @@ class Home extends React.Component{
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Carousel</a>
+      <a class="navbar-brand" href="#">VroomRoom</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -27,6 +28,10 @@ class Home extends React.Component{
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-outline-success" type="submit">Search</button>
+          <button  href="/signin" class="btn btn-outline-success" >Signin</button>
+          <button href="/signup"class="btn btn-outline-success" >SignUp</button>
+
+          
         </form>
       </div>
     </div>
