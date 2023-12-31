aedde05741be2a4fe10d2b15358488aec494347a
m0hx777 <m0hx@protonmail.com>
2023-12-31 13:58:23 +0300

    uwuntu

--- vroomroom-fe/src/App.js
+++ vroomroom-fe/src/App.js
@@ -11,7 +11,7 @@ function App() {
   
     <div>
       <Routes>
-        <Route path="/" element={ isAuth ? <AuthorList></AuthorList> : <Signin login={loginHandler}></Signin>}></Route>
+        <CategoryList></CategoryList>
       </Routes>
     </div>
 }
