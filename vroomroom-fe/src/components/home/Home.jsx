885a989a6cfa2149bdd9f5177808b47c1903fc82
m0hx777 <m0hx@protonmail.com>
2024-01-03 09:50:02 +0300

    uwuntu

--- vroomroom-fe/src/components/home/Home.jsx
+++ vroomroom-fe/src/components/home/Home.jsx
@@ -8,6 +8,9 @@ class Home extends React.Component{
         return (
           <div>
             <h1>Welcome Home</h1>
+            <h1>Welcome Home</h1>
+            <h1>Welcome Home</h1>
+            <h1>Welcome Home</h1>
 
           </div>
         )
